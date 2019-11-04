13a91eb99a4bb9c78d285b895855c10d14be0864
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2019-11-04 20:06:16 -0300

    V1 of App.class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        System.out.println("Bienvenido a supermecados 'TE SACAMOS UN OJO");
+        //variables temporales
+        int choiceCategory, choicequantity;
 
+        Scanner keyboard = new Scanner(System.in);
+
+        do {
+            System.out.println("Elija qué categoría de producto desea adquirir: (1) Comidas, " +
+                    "(2) Juguetes, (3) Ropa, (4) Herramientas");
+            choiceCategory = keyboard.nextInt();
+            switch (choiceCategory) {
+                case 1:
+                    System.out.println("Ingrese cuantos productos de esta categría desea llevar");
+                    choicequantity = keyboard.nextInt();
+                    for (int i = 0; i < choicequantity; i++) {
+
+                    }
+
+            }
+        // TODO: cambiar el true por la condicion apropiadad
+        } while (true);
     }
 }
