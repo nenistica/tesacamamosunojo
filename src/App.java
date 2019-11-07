d5ebb68c4409fca93174737da00c15f509991df8
Daiana Delvalle <52934488+daianadelvalle@users.noreply.github.com>
2019-11-07 15:24:26 -0300

    se realizó el do while para la elección de categorías y productos con la adición al arrayList de la clase carrito

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
 import ar.com.ada.store.ShoppingCart;
 import ar.com.ada.store.categories.Product;
+import ar.com.ada.store.categories.clothes.Clothes;
+import ar.com.ada.store.categories.clothes.Jacket;
+import ar.com.ada.store.categories.clothes.Pants;
+import ar.com.ada.store.categories.clothes.Tshirt;
 import ar.com.ada.store.categories.grocery.Bread;
 import ar.com.ada.store.categories.grocery.Flour;
 import ar.com.ada.store.categories.grocery.Grocery;
 import ar.com.ada.store.categories.grocery.Milk;
+import ar.com.ada.store.categories.tools.Hammer;
+import ar.com.ada.store.categories.tools.Nails;
+import ar.com.ada.store.categories.tools.Screwdriver;
+import ar.com.ada.store.categories.tools.Tools;
+import ar.com.ada.store.categories.toy.Ball;
+import ar.com.ada.store.categories.toy.Car;
+import ar.com.ada.store.categories.toy.Doll;
+import ar.com.ada.store.categories.toy.Toy;
 
 import java.util.Scanner;
 
@@ -12,6 +24,7 @@ public class App {
         System.out.println("Bienvenido a supermecados 'TE SACAMOS UN OJO");
         //variables temporales
         int choiceCategory, choicequantity, choiceProduct;
+        String answer;
 
         Scanner keyboard = new Scanner(System.in);
 
@@ -19,35 +32,126 @@ public class App {
 
         do {
             System.out.println("Elija qué categoría de producto desea adquirir: (1) Comidas, " +
-                    "(2) Juguetes, (3) Ropa, (4) Herramientas");
+                    "(2) Ropa, (3) Herramientas, (4) Juguetes");
             choiceCategory = keyboard.nextInt();
             switch (choiceCategory) {
                 case 1:
                     System.out.println("Ingrese cuantos productos de esta categría desea llevar");
                     choicequantity = keyboard.nextInt();
-                    for (int i = 0; i < choicequantity; i++) {
-                        System.out.println("Cual de los siguientes productos debe llevar? : leche (1), pan (2), harina (3)");
-                        choiceProduct = keyboard.nextInt();
-                        switch (choiceProduct) {
-                            case 1 :
-                                Grocery leche = new Milk();
-                                break;
-                            case 2:
-                                Grocery pan = new Bread();
-                                break;
-                            case 3:
-                                Grocery harina = new Flour();
-                                break;
-                            default:
-                                System.out.println();
+                    do {
+                        for (int i = 0; i < choicequantity; i++) {
+                            System.out.println("Cual de los siguientes productos debe llevar? : leche (1), pan (2), harina (3)");
+                            choiceProduct = keyboard.nextInt();
+                            switch (choiceProduct) {
+                                case 1:
+                                    Grocery leche = new Milk();
+                                    cart.addToProductList(leche);
+                                    break;
+                                case 2:
+                                    Grocery pan = new Bread();
+                                    cart.addToProductList(pan);
+                                    break;
+                                case 3:
+                                    Grocery harina = new Flour();
+                                    cart.addToProductList(harina);
+                                    break;
+                                default:
+                                    System.out.println("opción no disponible");
+                            }
                         }
+                        System.out.println("¿Desea llevar algo más?");
+                        answer = keyboard.next();
+                    } while (answer.equals("Sí"));
+                    break;
 
+                case 2: System.out.println("Ingrese cuantos productos de esta categría desea llevar");
+                    choicequantity = keyboard.nextInt();
+                    do {
+                        for (int i = 0; i < choicequantity; i++) {
+                            System.out.println("Cual de los siguientes productos debe llevar? : chaqueta (1), pantalón (2), remera (3)");
+                            choiceProduct = keyboard.nextInt();
+                            switch (choiceProduct) {
+                                case 1:
+                                    Clothes chaqueta = new Jacket();
+                                    cart.addToProductList(chaqueta);
+                                    break;
+                                case 2:
+                                    Clothes pantalon = new Pants();
+                                    cart.addToProductList(pantalon);
+                                    break;
+                                case 3:
+                                    Clothes remera = new Tshirt();
+                                    cart.addToProductList(remera);
+                                    break;
+                                default:
+                                    System.out.println("opción no disponible");
+                            }
+                        }
+                        System.out.println("¿Desea llevar algo más?");
+                        answer = keyboard.next();
+                    } while (answer.equals("Sí")) ;
+                    break;
 
-
-                    }
+                case 3: System.out.println("Ingrese cuantos productos de esta categría desea llevar");
+                    choicequantity = keyboard.nextInt();
+                    do {
+                        for (int i = 0; i < choicequantity; i++) {
+                            System.out.println("Cual de los siguientes productos debe llevar? : martillo (1), clavos (2), destornillador (3)");
+                            choiceProduct = keyboard.nextInt();
+                            switch (choiceProduct) {
+                                case 1:
+                                    Tools martillo = new Hammer();
+                                    cart.addToProductList(martillo);
+                                    break;
+                                case 2:
+                                    Tools clavos = new Nails();
+                                    cart.addToProductList(clavos);
+                                    break;
+                                case 3:
+                                    Tools destornillador = new Screwdriver();
+                                    cart.addToProductList(destornillador);
+                                    break;
+                                default:
+                                    System.out.println("opción no disponible");
+                            }
+                        }
+                        System.out.println("¿Desea llevar algo más?");
+                        answer = keyboard.next();
+                    } while (answer.equals("Sí")) ;
+                    break;
+                case 4: System.out.println("Ingrese cuantos productos de esta categría desea llevar");
+                    choicequantity = keyboard.nextInt();
+                    do {
+                        for (int i = 0; i < choicequantity; i++) {
+                            System.out.println("Cual de los siguientes productos debe llevar? : pelota (1), auto (2), muñeca (3)");
+                            choiceProduct = keyboard.nextInt();
+                            switch (choiceProduct) {
+                                case 1:
+                                    Toy pelota = new Ball();
+                                    cart.addToProductList(pelota);
+                                    break;
+                                case 2:
+                                    Toy auto = new Car();
+                                    cart.addToProductList(auto);
+                                    break;
+                                case 3:
+                                    Clothes muñeca = new Doll();
+                                    cart.addToProductList(muñeca);
+                                    break;
+                                default:
+                                    System.out.println("opción no disponible");
+                            }
+                        }
+                        System.out.println("¿Desea llevar algo más?");
+                        answer = keyboard.next();
+                    } while (answer.equals("Sí")) ;
+                    break;
 
             }
-        // TODO: cambiar el true por la condicion apropiadad
-        } while (true);
+            System.out.println("¿Desea comprar algo más? Tipee 'si' o 'no'.");
+            answer = keyboard.next();
+        } while (answer.equals("si"));
     }
+
+
 }
