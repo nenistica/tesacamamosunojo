cb7c3971f2d4671cd8168ab994560ea536cadef1
gabrielachachond <gabrielachacond@gmail.com>
2019-11-08 20:55:07 -0300

    subir la clase abstracta de ropa

--- src/ar/com/ada/store/categories/clothes/Clothes.java
+++ src/ar/com/ada/store/categories/clothes/Clothes.java
@@ -1,4 +1,28 @@
 package ar.com.ada.store.categories.clothes;
 
-public class Clothes {
+public abstract class Clothes extends Product {
+
+    public void Ropa(Double monto, Double priceList) {
+        this.monto = priceList;
+        this.priceList = priceList;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setPriceList(Double priceList) {
+        this.priceList = priceList;
+    }
+
+    public Double getPriceList() {
+        return priceList;
+    }
+
+    @Override
+    public String calcularDescuento(){
+        if (this.priceList >=   && this.priceList <= 15%){
+            return "El descuento de este producto es:";
+        }
+
 }
